4f86e493b7a60eea6f2a28fe00637ee8f559edd9
sj1978 <daniel_amoroso31@hotmail.com>
2022-03-15 12:46:39 -0300

    added requested changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  resources :users do
-  resources :posts
-  end 
-
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
