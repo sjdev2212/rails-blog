ceaab45b6634b115e6452928fc458eafcaba30f5
sj1978 <daniel_amoroso31@hotmail.com>
2022-03-16 23:08:57 -0300

    fix migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_09_185416) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_17_020620) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,13 +34,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_09_185416) do
   end
 
   create_table "posts", force: :cascade do |t|
+    t.bigint "author_id"
     t.string "title"
     t.text "text"
+    t.text "bio"
     t.integer "comments_counter"
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "author_id", null: false
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
@@ -53,13 +54,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_09_185416) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "widgets", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_foreign_key "comments", "users", column: "author_id"
-  add_foreign_key "likes", "users", column: "author_id"
-  add_foreign_key "posts", "users", column: "author_id"
 end
